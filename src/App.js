dc7732b1d1c732dd8f991be959d75bba85d7fa41
filipesenaifirjan <filipemattossenai@gmail.com>
2022-02-14 20:37:08 -0300

    input-novo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+
+ 
+  
+  return( 
+    <form>
+  <label>
+    Nome:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Enviar" />
+</form>
+ 
+    );
+  }
+
+export default App;
